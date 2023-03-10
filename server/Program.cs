0b1d167b927137f78644a90c474765a9529c90a1
Gary Sharp <7181962+mlancer@users.noreply.github.com>
2023-03-09 21:47:52 -0500

    changes needed for inital publish to azure for better collab

--- server/Program.cs
+++ server/Program.cs
@@ -19,11 +19,11 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
