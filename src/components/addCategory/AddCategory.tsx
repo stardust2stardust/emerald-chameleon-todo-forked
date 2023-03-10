e7db1bebcee9f82a87545ddedf6787bddbe9f347
Ziah Rahl <ziahrahl@gmail.com>
2023-03-10 16:24:55 -0500

    add UI for error messages

--- src/components/addCategory/AddCategory.tsx
+++ src/components/addCategory/AddCategory.tsx
@@ -10,56 +10,95 @@ type categoryObject = {
 const AddCategory = () => {
   // state of Add Category button
   const [isClicked, setIsClicked] = useState(false)
-  // const [categoryList, setCategoryList] = useState([])
+  const [isExistingCategory, setIsExistingCategory] = useState(false)
+  const [isOtherError, setIsOtherError] = useState(false)
+  const [isEmptyInput, setIsEmptyInput] = useState(false)
+
+  // text input
 
   // 'https://todobackend20230309204702.azurewebsites.net/api/category' this is the endpoint for Categories.
   //navigating to https://todobackend20230309204702.azurewebsites.net/swagger/index.html will allow you to interrogate all the endpoints in the backend.
+
   const categoryUrl =
-    'https://todobackend20230309204702.azurewebsites.net/api/categor'
+    'https://todobackend20230309204702.azurewebsites.net/api/category'
 
   // isClicked state changes when clicking on Add Category, Ok, or Cancel buttons
   const handleClick = () => {
-    console.log('you clicked a button')
     setIsClicked(!isClicked)
   }
 
-  const getCategoryName = () => {
-    // get value of text input
+  const handleCancelClick = () => {
+    setIsExistingCategory(false)
+    setIsOtherError(false)
+    setIsClicked(!isClicked)
     const newCategoryInput = document.getElementById(
       'new-category-text'
     ) as HTMLInputElement
-
-    const categoryName: string = newCategoryInput.value
-    return categoryName
+    newCategoryInput.value = ''
   }
 
-  const addNewCategory = async () => {
-    const newCategoryName: string = getCategoryName()
-    handleClick()
+  const getCategoryName = () => {
+    const newCategoryInput = document.getElementById(
+      'new-category-text'
+    ) as HTMLInputElement
+    const categoryName: string = newCategoryInput.value
+    if (categoryName === '' || categoryName === undefined) {
+      console.log('please enter a category name')
+      setIsEmptyInput(true)
+    } else {
+      addNewCategory(categoryName)
+    }
+  }
+
+  const addNewCategory = async (newCategoryName: string) => {
     try {
       const response = await fetch(categoryUrl)
+      console.log(response)
       const categoryObjArray = await response.json()
       console.log(categoryObjArray)
       categoryObjArray.map((obj: categoryObject) => {
-        console.log(obj.name)
         if (obj.name.toLowerCase() === newCategoryName.toLowerCase()) {
-          console.log('that Category exists')
+          console.log('that Category exists already')
+          setIsExistingCategory(true)
+        } else {
+          addCategoryToArray(newCategoryName)
         }
       })
       if (!response.ok) {
         console.log('Could not get data')
+        setIsOtherError(true)
         return
       }
     } catch (error) {
       console.log(error)
     }
-    addCategoryToArray(newCategoryName)
   }
 
-  const addCategoryToArray = (categoryName: string) => {
+  const addCategoryToArray = async (categoryName: string) => {
     console.log(categoryName)
+    const newCategory = {
+      // catId: id,
+      name: categoryName,
+    }
+    try {
+      const response = await fetch(categoryUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newCategory),
+      })
+
+      if (!response.ok) {
+        console.log('error in POST fetch')
+        setIsOtherError(true)
+        return
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
-  // add Category (newCategoryInput.value) to existing List
+
   // <<<<<<<<<<<<<<<<<<<<<<<<<
   ///Travis Comments>>>
   ///So we should be able to rely on our backend/database to ensure data integrity, 
@@ -74,27 +113,38 @@ const AddCategory = () => {
   //};
   // axios.post(todoCategoryUrl, newCategoryPayload)
   //   .then() - handle what you want to do here (updating the display)
-  //   .catch(error) - the category was not written to the db, handle what to do in that case here 
+  //   .catch(error) - the category was not written to the db, handle what to do in that case here
   // !!There could be many reasons the HTTP request fails, "bonus points" if you display a message to the user the category already exists 
   // and a generic error message for any other error (like the backend service being down, backend has no connection to db, etc...). 
   // "Exception handling is the difference between good programmers and *exceptional* programmers".
- // >>>>>>>>>>>>>>>>>>>>>>>>>>
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>
 
   return (
     <div>
       {isClicked ? (
         <div className="new-category-input-wrapper">
+          <div className="error-message">
+            {isExistingCategory && <p>That category already exists.</p>}
+          </div>
+          <div className="error-message">
+            {isEmptyInput && <p>Please enter a category name.</p>}
+          </div>
+          <div className="error-message">
+            {isOtherError && (
+              <p>Uh oh! There is a connection issue to the server. :/</p>
+            )}
+          </div>
           <input id="new-category-text" type="text" />
           <div className="new-category-input-buttons">
             <button
               id="okBtn"
               className="btn-ok"
-              onClick={addNewCategory}
+              onClick={getCategoryName}
               type="submit"
             >
               Ok
             </button>
-            <button className="btn-cancel" onClick={handleClick}>
+            <button className="btn-cancel" onClick={handleCancelClick}>
               Cancel
             </button>
           </div>
